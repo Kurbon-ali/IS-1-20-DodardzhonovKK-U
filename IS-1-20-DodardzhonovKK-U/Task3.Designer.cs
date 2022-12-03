4293767d7661ddec5107395e05d8c1718c822364
Курбонали Додарджонов <chocirov@mail.ru>
2022-12-03 10:59:37 +0500

    Начал делать

--- IS-1-20-DodardzhonovKK-U/Task3.Designer.cs
+++ IS-1-20-DodardzhonovKK-U/Task3.Designer.cs
@@ -29,12 +29,33 @@ namespace IS_1_20_DodardzhonovKK_U
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(127, 82);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Task3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(633, 350);
+            this.Controls.Add(this.button1);
+            this.Name = "Task3";
             this.Text = "Task3";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
     }
 }
--- IS-1-20-DodardzhonovKK-U/Task3.cs
+++ IS-1-20-DodardzhonovKK-U/Task3.cs
@@ -7,14 +7,34 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace IS_1_20_DodardzhonovKK_U
 {
     public partial class Task3 : Form
     {
+
+        //Переменная соединения
+        MySqlConnection conn;
+        //DataAdapter представляет собой объект Command , получающий данные из источника данных.
+        private MySqlDataAdapter MyDA = new MySqlDataAdapter();
+        //Объявление BindingSource, основная его задача, это обеспечить унифицированный доступ к источнику данных.
+        private BindingSource bSource = new BindingSource();
+        
+        private DataSet ds = new DataSet();
+        //Представляет одну таблицу данных в памяти.
+        private DataTable table = new DataTable();
+
+        
+
         public Task3()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
