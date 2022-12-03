ebbf2c72c278cd626ba9a432d60fa46c1dd4d575
Курбонали Додарджонов <chocirov@mail.ru>
2022-12-03 08:13:12 +0500

    Первое задание сделано!

--- IS-1-20-DodardzhonovKK-U/Task2.cs
+++ IS-1-20-DodardzhonovKK-U/Task2.cs
@@ -7,14 +7,24 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace IS_1_20_DodardzhonovKK_U
 {
     public partial class Task2 : Form
     {
+        class MySqlConnect
+        {
+
+        }
         public Task2()
         {
             InitializeComponent();
+        }
+
+        private void Task2_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
