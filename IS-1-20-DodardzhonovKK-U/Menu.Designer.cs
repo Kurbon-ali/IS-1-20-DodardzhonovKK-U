003cb5dbd1b520d4ca0b5c4617dcf567e7fb78dc
Курбонали Додарджонов <chocirov@mail.ru>
2022-12-03 08:46:20 +0500

    Меню

--- IS-1-20-DodardzhonovKK-U/Menu.Designer.cs
+++ IS-1-20-DodardzhonovKK-U/Menu.Designer.cs
@@ -42,7 +42,7 @@ namespace IS_1_20_DodardzhonovKK_U
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(101, 64);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "Задание 1";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
@@ -52,7 +52,7 @@ namespace IS_1_20_DodardzhonovKK_U
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(101, 64);
             this.button2.TabIndex = 1;
-            this.button2.Text = "button2";
+            this.button2.Text = "Задание 2";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
@@ -62,7 +62,7 @@ namespace IS_1_20_DodardzhonovKK_U
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(101, 64);
             this.button3.TabIndex = 2;
-            this.button3.Text = "button3";
+            this.button3.Text = "Задание 3";
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
@@ -72,7 +72,7 @@ namespace IS_1_20_DodardzhonovKK_U
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(101, 64);
             this.button4.TabIndex = 3;
-            this.button4.Text = "button4";
+            this.button4.Text = "Задание 4";
             this.button4.UseVisualStyleBackColor = true;
             this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
@@ -82,7 +82,7 @@ namespace IS_1_20_DodardzhonovKK_U
             this.button5.Name = "button5";
             this.button5.Size = new System.Drawing.Size(101, 64);
             this.button5.TabIndex = 4;
-            this.button5.Text = "button5";
+            this.button5.Text = "Задание 5";
             this.button5.UseVisualStyleBackColor = true;
             this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
