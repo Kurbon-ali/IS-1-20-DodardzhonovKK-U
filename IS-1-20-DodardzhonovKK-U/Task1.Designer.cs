50fbae5531a93371a3150f67ae89e5ae328a1177
Курбонали Додарджонов <chocirov@mail.ru>
2022-12-06 22:25:14 +0500

    Первое задание сделано!

--- IS-1-20-DodardzhonovKK-U/Task1.Designer.cs
+++ IS-1-20-DodardzhonovKK-U/Task1.Designer.cs
@@ -56,35 +56,40 @@ namespace IS_1_20_DodardzhonovKK_U
             // listBox1
             // 
             this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(16, 325);
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(21, 400);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(660, 82);
+            this.listBox1.Size = new System.Drawing.Size(879, 100);
             this.listBox1.TabIndex = 0;
             this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(290, 12);
+            this.textBox1.Location = new System.Drawing.Point(387, 15);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(176, 20);
+            this.textBox1.Size = new System.Drawing.Size(233, 24);
             this.textBox1.TabIndex = 1;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(290, 48);
+            this.textBox2.Location = new System.Drawing.Point(387, 59);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox2.Multiline = true;
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(176, 18);
+            this.textBox2.Size = new System.Drawing.Size(233, 21);
             this.textBox2.TabIndex = 2;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label1.Location = new System.Drawing.Point(184, 46);
+            this.label1.Location = new System.Drawing.Point(245, 57);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(48, 20);
+            this.label1.Size = new System.Drawing.Size(58, 25);
             this.label1.TabIndex = 3;
             this.label1.Text = "Цена";
             // 
@@ -92,17 +97,19 @@ namespace IS_1_20_DodardzhonovKK_U
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label2.Location = new System.Drawing.Point(184, 86);
+            this.label2.Location = new System.Drawing.Point(245, 106);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(103, 20);
+            this.label2.Size = new System.Drawing.Size(126, 25);
             this.label2.TabIndex = 4;
             this.label2.Text = "Год выпуска";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(180, 278);
+            this.button1.Location = new System.Drawing.Point(240, 342);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(76, 31);
+            this.button1.Size = new System.Drawing.Size(101, 38);
             this.button1.TabIndex = 5;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
@@ -110,9 +117,10 @@ namespace IS_1_20_DodardzhonovKK_U
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(450, 278);
+            this.button2.Location = new System.Drawing.Point(600, 342);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(76, 31);
+            this.button2.Size = new System.Drawing.Size(101, 38);
             this.button2.TabIndex = 6;
             this.button2.Text = "button2";
             this.button2.UseVisualStyleBackColor = true;
@@ -120,60 +128,68 @@ namespace IS_1_20_DodardzhonovKK_U
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(290, 88);
+            this.textBox3.Location = new System.Drawing.Point(387, 108);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(176, 20);
+            this.textBox3.Size = new System.Drawing.Size(233, 22);
             this.textBox3.TabIndex = 7;
             // 
             // textBox4
             // 
-            this.textBox4.Location = new System.Drawing.Point(201, 137);
+            this.textBox4.Location = new System.Drawing.Point(268, 169);
+            this.textBox4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(147, 20);
+            this.textBox4.Size = new System.Drawing.Size(195, 22);
             this.textBox4.TabIndex = 8;
             // 
             // textBox5
             // 
-            this.textBox5.Location = new System.Drawing.Point(201, 182);
+            this.textBox5.Location = new System.Drawing.Point(268, 224);
+            this.textBox5.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(147, 20);
+            this.textBox5.Size = new System.Drawing.Size(195, 22);
             this.textBox5.TabIndex = 9;
             // 
             // textBox6
             // 
-            this.textBox6.Location = new System.Drawing.Point(201, 230);
+            this.textBox6.Location = new System.Drawing.Point(268, 283);
+            this.textBox6.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox6.Name = "textBox6";
-            this.textBox6.Size = new System.Drawing.Size(147, 20);
+            this.textBox6.Size = new System.Drawing.Size(195, 22);
             this.textBox6.TabIndex = 10;
             // 
             // textBox7
             // 
-            this.textBox7.Location = new System.Drawing.Point(520, 135);
+            this.textBox7.Location = new System.Drawing.Point(693, 166);
+            this.textBox7.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox7.Name = "textBox7";
-            this.textBox7.Size = new System.Drawing.Size(165, 20);
+            this.textBox7.Size = new System.Drawing.Size(219, 22);
             this.textBox7.TabIndex = 11;
             // 
             // textBox8
             // 
-            this.textBox8.Location = new System.Drawing.Point(520, 184);
+            this.textBox8.Location = new System.Drawing.Point(693, 226);
+            this.textBox8.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox8.Name = "textBox8";
-            this.textBox8.Size = new System.Drawing.Size(165, 20);
+            this.textBox8.Size = new System.Drawing.Size(219, 22);
             this.textBox8.TabIndex = 12;
             // 
             // textBox9
             // 
-            this.textBox9.Location = new System.Drawing.Point(520, 230);
+            this.textBox9.Location = new System.Drawing.Point(693, 283);
+            this.textBox9.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox9.Name = "textBox9";
-            this.textBox9.Size = new System.Drawing.Size(165, 20);
+            this.textBox9.Size = new System.Drawing.Size(219, 22);
             this.textBox9.TabIndex = 13;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label3.Location = new System.Drawing.Point(184, 12);
+            this.label3.Location = new System.Drawing.Point(245, 15);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(72, 20);
+            this.label3.Size = new System.Drawing.Size(90, 25);
             this.label3.TabIndex = 14;
             this.label3.Text = "Артикул";
             // 
@@ -181,9 +197,10 @@ namespace IS_1_20_DodardzhonovKK_U
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label4.Location = new System.Drawing.Point(12, 137);
+            this.label4.Location = new System.Drawing.Point(16, 169);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(183, 20);
+            this.label4.Size = new System.Drawing.Size(226, 25);
             this.label4.TabIndex = 15;
             this.label4.Text = "Количество Оборотов:";
             // 
@@ -191,9 +208,10 @@ namespace IS_1_20_DodardzhonovKK_U
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label5.Location = new System.Drawing.Point(12, 182);
+            this.label5.Location = new System.Drawing.Point(16, 224);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(97, 20);
+            this.label5.Size = new System.Drawing.Size(122, 25);
             this.label5.TabIndex = 16;
             this.label5.Text = "Интерфейс";
             // 
@@ -201,9 +219,10 @@ namespace IS_1_20_DodardzhonovKK_U
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label6.Location = new System.Drawing.Point(12, 230);
+            this.label6.Location = new System.Drawing.Point(16, 283);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(61, 20);
+            this.label6.Size = new System.Drawing.Size(78, 25);
             this.label6.TabIndex = 17;
             this.label6.Text = "Объем";
             // 
@@ -211,9 +230,10 @@ namespace IS_1_20_DodardzhonovKK_U
             // 
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label7.Location = new System.Drawing.Point(384, 135);
+            this.label7.Location = new System.Drawing.Point(512, 166);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(74, 20);
+            this.label7.Size = new System.Drawing.Size(90, 25);
             this.label7.TabIndex = 18;
             this.label7.Text = "Частота";
             // 
@@ -221,9 +241,10 @@ namespace IS_1_20_DodardzhonovKK_U
             // 
             this.label8.AutoSize = true;
             this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label8.Location = new System.Drawing.Point(384, 182);
+            this.label8.Location = new System.Drawing.Point(512, 224);
+            this.label8.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(131, 20);
+            this.label8.Size = new System.Drawing.Size(159, 25);
             this.label8.TabIndex = 19;
             this.label8.Text = "Производитель";
             // 
@@ -231,27 +252,29 @@ namespace IS_1_20_DodardzhonovKK_U
             // 
             this.label9.AutoSize = true;
             this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label9.Location = new System.Drawing.Point(384, 230);
+            this.label9.Location = new System.Drawing.Point(512, 283);
+            this.label9.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(124, 20);
+            this.label9.Size = new System.Drawing.Size(158, 25);
             this.label9.TabIndex = 20;
             this.label9.Text = "Объем Памяти";
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(592, 361);
+            this.button3.Location = new System.Drawing.Point(789, 444);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(84, 46);
+            this.button3.Size = new System.Drawing.Size(112, 57);
             this.button3.TabIndex = 21;
-            this.button3.Text = "Exit";
+            this.button3.Text = "Back";
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // Task1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(688, 419);
+            this.ClientSize = new System.Drawing.Size(917, 516);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.label9);
             this.Controls.Add(this.label8);
@@ -274,6 +297,7 @@ namespace IS_1_20_DodardzhonovKK_U
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.listBox1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Task1";
             this.Text = "Task1";
             this.Load += new System.EventHandler(this.Task1_Load);
