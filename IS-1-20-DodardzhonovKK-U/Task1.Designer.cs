6e47d3d2143c89c0c5e37321d86f69aed0ca3ba3
Курбонали Додарджонов <chocirov@mail.ru>
2022-12-06 22:26:29 +0500

    Первое задание сделано!

--- IS-1-20-DodardzhonovKK-U/Task1.Designer.cs
+++ IS-1-20-DodardzhonovKK-U/Task1.Designer.cs
@@ -111,7 +111,7 @@ namespace IS_1_20_DodardzhonovKK_U
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(101, 38);
             this.button1.TabIndex = 5;
-            this.button1.Text = "button1";
+            this.button1.Text = "Hard Drives";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
@@ -122,7 +122,7 @@ namespace IS_1_20_DodardzhonovKK_U
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(101, 38);
             this.button2.TabIndex = 6;
-            this.button2.Text = "button2";
+            this.button2.Text = "Videocard";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
