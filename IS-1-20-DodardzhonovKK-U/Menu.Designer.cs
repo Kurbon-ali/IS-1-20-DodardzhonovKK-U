46c67df64e5c20275e0bc1b7337f87e8335688b2
Курбонали Додарджонов <chocirov@mail.ru>
2022-12-06 22:24:02 +0500

    Меню готово!

--- IS-1-20-DodardzhonovKK-U/Menu.Designer.cs
+++ IS-1-20-DodardzhonovKK-U/Menu.Designer.cs
@@ -39,9 +39,10 @@ namespace IS_1_20_DodardzhonovKK_U
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(66, 43);
+            this.button1.Location = new System.Drawing.Point(88, 53);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(101, 64);
+            this.button1.Size = new System.Drawing.Size(135, 79);
             this.button1.TabIndex = 0;
             this.button1.Text = "Задание 1";
             this.button1.UseVisualStyleBackColor = true;
@@ -49,9 +50,10 @@ namespace IS_1_20_DodardzhonovKK_U
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(238, 43);
+            this.button2.Location = new System.Drawing.Point(317, 53);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(101, 64);
+            this.button2.Size = new System.Drawing.Size(135, 79);
             this.button2.TabIndex = 1;
             this.button2.Text = "Задание 2";
             this.button2.UseVisualStyleBackColor = true;
@@ -59,9 +61,10 @@ namespace IS_1_20_DodardzhonovKK_U
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(427, 43);
+            this.button3.Location = new System.Drawing.Point(539, 53);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(101, 64);
+            this.button3.Size = new System.Drawing.Size(135, 79);
             this.button3.TabIndex = 2;
             this.button3.Text = "Задание 3";
             this.button3.UseVisualStyleBackColor = true;
@@ -69,9 +72,10 @@ namespace IS_1_20_DodardzhonovKK_U
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(66, 173);
+            this.button4.Location = new System.Drawing.Point(88, 213);
+            this.button4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(101, 64);
+            this.button4.Size = new System.Drawing.Size(135, 79);
             this.button4.TabIndex = 3;
             this.button4.Text = "Задание 4";
             this.button4.UseVisualStyleBackColor = true;
@@ -79,9 +83,10 @@ namespace IS_1_20_DodardzhonovKK_U
             // 
             // button5
             // 
-            this.button5.Location = new System.Drawing.Point(427, 173);
+            this.button5.Location = new System.Drawing.Point(317, 213);
+            this.button5.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(101, 64);
+            this.button5.Size = new System.Drawing.Size(135, 79);
             this.button5.TabIndex = 4;
             this.button5.Text = "Задание 5";
             this.button5.UseVisualStyleBackColor = true;
@@ -89,25 +94,27 @@ namespace IS_1_20_DodardzhonovKK_U
             // 
             // button6
             // 
-            this.button6.Location = new System.Drawing.Point(454, 271);
+            this.button6.Location = new System.Drawing.Point(575, 271);
+            this.button6.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(74, 45);
+            this.button6.Size = new System.Drawing.Size(99, 55);
             this.button6.TabIndex = 5;
-            this.button6.Text = "Выход";
+            this.button6.Text = "Exit";
             this.button6.UseVisualStyleBackColor = true;
             this.button6.Click += new System.EventHandler(this.button6_Click);
             // 
             // Menu
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(570, 328);
+            this.ClientSize = new System.Drawing.Size(716, 364);
             this.Controls.Add(this.button6);
             this.Controls.Add(this.button5);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Menu";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Menu_Load);
