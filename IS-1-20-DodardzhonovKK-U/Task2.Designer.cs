79d4bb89d48f64a3fdb3b450622acccf50055047
Курбонали Додарджонов <chocirov@mail.ru>
2022-12-06 22:28:18 +0500

    Второе задание сделано!

--- IS-1-20-DodardzhonovKK-U/Task2.Designer.cs
+++ IS-1-20-DodardzhonovKK-U/Task2.Designer.cs
@@ -35,31 +35,35 @@ namespace IS_1_20_DodardzhonovKK_U
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(16, 15);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(116, 41);
+            this.button1.Size = new System.Drawing.Size(193, 81);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "check";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(124, 59);
+            this.button2.Location = new System.Drawing.Point(228, 114);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(126, 68);
+            this.button2.Size = new System.Drawing.Size(105, 43);
             this.button2.TabIndex = 1;
-            this.button2.Text = "Exit";
+            this.button2.Text = "Back";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // Task2
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(256, 146);
+            this.ClientSize = new System.Drawing.Size(341, 180);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Task2";
             this.Text = "Task2";
             this.Load += new System.EventHandler(this.Task2_Load);
